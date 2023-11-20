83d24bef77396f10d044745f945433fdd5bf98e8
Bishal De <itsbishalde@gmail.com>
2023-11-20 22:55:27 +0530

    added routes

--- index.js
+++ index.js
@@ -1,31 +1,38 @@
-const express = require('express')
-const path = require('path')
+const express = require("express");
+const path = require("path");
 
+const app = express();
 
-const app = express()
-
-
-const PORT  = 3000
+const PORT = 3000;
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-
-    const data = {
-        1:"",
-        2:"/week5"
-    }
-    res.send(data)
+app.get("/", (req, res) => {
+  const data = {
+    1: "/week1",
+    2: "/week2",
+    3: "/week3",
+    4: "/week4",
+    5: "/week5",
+    6: "/week6",
+    7: "/week7",
+    8: "/week8",
+    9: "/week9",
+  };
+  res.send(data);
 });
 
-
-app.get('/week5', (req, res) => {
-    const filePath = path.join(__dirname, 'Files/test.html');
+app.get("/week:id", (req, res) => {
+  const weekNumber = req.params.id;
+  if (weekNumber >= 1 && weekNumber <= 9) {
+    const filePath = path.join(__dirname, `Files/week${weekNumber}.html`);
     res.sendFile(filePath);
+  } else {
+    res.status(404).send("Week not found");
+  }
 });
 
 
 app.listen(PORT, () => {
-    console.log(`Server listening on http://127.0.0.1:${PORT}`);
-  });
-  
+  console.log(`Server listening on http://127.0.0.1:${PORT}`);
+});
